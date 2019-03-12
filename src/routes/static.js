45bcd24a4d4b99084ace46ffe7c6e24883e81f15
unknown <janais1991@gmail.com>
2019-03-12 17:12:28 -0500

    In progress; npm test fail

--- src/routes/static.js
+++ src/routes/static.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const staticController = require("../controllers/staticController");
 
-router.get("/", (req, res, next) => {
-  res.send("Welcome to Bloccit, polo");
+router.get("/", staticController.index);
+
+router.get("/marco", (req, res, next) => {
+  //marco polo
 });
 
 module.exports = router;
