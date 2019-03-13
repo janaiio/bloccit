c37b5f6d6d0d744edfd448a2e8e94192310f3192
unknown <janais1991@gmail.com>
2019-03-13 18:05:52 -0500

    Checkpoint in progress; At Associations now

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   init(app){ //loads defined routes and defines them on the Express app object
     const staticRoutes = require("../routes/static");
+    const topicRoutes = require("../routes/topics");
+
     app.use(staticRoutes);
+    app.use(topicRoutes);
   }
 }
