b3daddee636982d7f600be18827fe090b4702bcc
unknown <janais1991@gmail.com>
2019-03-13 19:24:20 -0500

    Checkpoint in progress; Sequelize error 'relation Topic doesn't exist'

---
+++ src/db/models/banner.js
@@ -0,0 +1,23 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Banner = sequelize.define('Banner', {
+    source: DataTypes.STRING,
+    description: DataTypes.STRING,
+    topicID: {
+      type: DataTypes.INTEGER,
+      onDelete: "CASCADE",
+      references: {
+        model: "Topic",
+        key: "id",
+        as: "topicID",
+      }
+    }
+  }, {});
+  Banner.associate = function(models) {
+    Banner.belongsTo(models.Topic, {
+      foreignKey: "topicID",
+      onDelete: "CASCADE"
+    });
+  };
+  return Banner;
+};
